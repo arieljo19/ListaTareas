404be54b265937b690cabd11ca4b6815dc8abe8e
arieljo19 <54317246+arieljo19@users.noreply.github.com>
2020-11-23 11:20:01 -0300

    inicial

---
+++ app/src/main/java/com/ajara/listatareas/WriteText.java
@@ -0,0 +1,14 @@
+package com.ajara.listatareas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriteText extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_text);
+    }
+}
